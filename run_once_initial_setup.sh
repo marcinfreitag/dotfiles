a76c89881c3727cf035fdb28fe65093443cbbef5
Marcin Freitag <marcin.freitag@outlook.com>
2024-03-01 12:22:28 +0100

    fixed typo in run_once script

--- run_once_initial_setup.sh
+++ run_once_initial_setup.sh
@@ -8,7 +8,7 @@ if [ ! -f /etc/yum.repos.d/rpmfusion-free.repo ]; then
     sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 fi
 if [ ! -f /etc/yum.repos.d/rpmfusion-nonfree.repo ]; then
-    sudo dnf -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rp
+    sudo dnf -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 fi
 if [ ! -f /etc/yum.repos.d/brave-browser.repo ]; then
     sudo dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
