8d5ca3bc445e8ef8bd7875b2271536af91c0ff62
Marcin Freitag <marcin.freitag@outlook.com>
2024-03-01 10:33:11 +0100

    split run script for easier maintenance

--- run_once_initial_setup.sh
+++ run_once_initial_setup.sh
@@ -4,32 +4,18 @@ echo "Removing unneeded software packages"
 sudo dnf -y remove baobab gnome-abrt gnome-logs gnome-maps gnome-tour gnome-weather mediawriter totem && sudo dnf -y autoremove
 
 echo "Adding package repos"
-sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf -y config-manager --add-repo .local/share/chezmoi/provisioning/teamviewer.repo
-sudo rpm --import https://linux.teamviewer.com/pubkey/currentkey.asc
+if [ -f /etc/yum.repos.d/rpmfusion-free.repo ]; then
+    sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+fi
+if [ -f /etc/yum.repos.d/rpmfusion-nonfree.repo ]; then
+    sudo dnf -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rp
+fi
+if [ -f /etc/yum.repos.d/brave-browser.repo ]; then
+    sudo dnf -y config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+	sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+fi
+if [ -f /etc/yum.repos.d/teamviewer.repo ]; then
+	sudo dnf -y config-manager --add-repo .local/share/chezmoi/provisioning/teamviewer.repo
+	sudo rpm --import https://linux.teamviewer.com/pubkey/currentkey.asc
+fi
 
-echo "Upgrading system"
-sudo dnf -y clean all && sudo dnf -y group upgrade --with-optional --allowerasing Multimedia && sudo dnf -y upgrade --refresh
-~/.local/bin/update-fedora.sh -y
-
-echo "Installing software packages"
-sudo dnf -y install celluloid clamav dconf-editor flatseal geary gimp gnome-shell-extension-caffeine gnome-shell-extension-no-overview gnome-extensions-app htop inkscape langpacks-de langpacks-fr langpacks-nl langpacks-pl lynis neofetch mpv neovim pass pass-otp syncthing tldr zbar teamviewer  brave-browser
-sudo dnf -y install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-
-echo "Installing language support for flatpaks"
-sudo flatpak config --set languages "en;de;pl;fr;nl"
-
-echo "Running update, just to make shure"
-~/.local/bin/update-fedora.sh -y
-
-echo "Enabling gpg-agent"
-systemctl --user enable --now gpg-agent.socket
-systemctl --user enable --now gpg-agent-ssh.socket
-
-echo "Setting default browser"
-xdg-settings set default-web-browser brave-browser.desktop
-
-echo "Loading dconf settings"
-cat ~/.local/share/chezmoi/provisioning/dconf-settings.ini | dconf load /
---
+++ run_onchange_load-settings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Upgrading system"
+sudo dnf -y clean all && sudo dnf -y group upgrade --with-optional --allowerasing Multimedia && sudo dnf -y upgrade --refresh
+~/.local/bin/update-fedora.sh -y
+
+echo "Installing software packages"
+sudo dnf -y install age celluloid clamav dconf-editor flatseal geary gimp gnome-shell-extension-caffeine gnome-shell-extension-no-overview gnome-extensions-app htop inkscape langpacks-de langpacks-fr langpacks-nl langpacks-pl lynis neofetch mpv neovim pass pass-otp syncthing tldr zbar teamviewer  brave-browser
+sudo dnf -y install https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
