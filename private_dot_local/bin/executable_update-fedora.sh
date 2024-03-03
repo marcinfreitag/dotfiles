d6fec8408b1b567d41da4ddb787edd9afe080fe8
Marcin Freitag <marcin.freitag@outlook.com>
2024-03-03 11:26:06 +0100

    fixed fedora-update script

--- private_dot_local/bin/executable_update-fedora.sh
+++ private_dot_local/bin/executable_update-fedora.sh
@@ -6,16 +6,21 @@ usage() { echo -e "Usage: $0 [-y]\n$0 update system interactively\n$0 -y
 }
 
 # parameters for noninteractive setup
-yes_dnf="" yes_flatpak=""
+yes_dnf=""
+yes_flatpak=""
 
 while getopts "y" options; do case "${options}" in
         y)
             if [ ! -z "${OPTARG}" ] ; then usage
-            fi yes_dnf="-y" yes_flatpak="--noninteractive"
+            fi
+	    yes_dnf="-y"
+	    yes_flatpak="--noninteractive"
             ;;
         *) usage
             ;;
-    esac done shift $((OPTIND-1))
+    esac
+done
+shift $((OPTIND-1))
 
 
 sudo dnf $yes_dnf upgrade --refresh; flatpak update $yes_flatpak
