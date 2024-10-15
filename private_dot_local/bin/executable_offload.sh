e928f904cc42f4853cb78bda4da2b9af214bd0ee
Marcin Freitag <marcin.freitag@outlook.com>
2024-10-15 19:40:06 +0200

    added script for offloading to nvidia card

---
+++ private_dot_local/bin/executable_offload.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# simple script to offload the program given as arg to render by nvidia gpu
+
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __VK_LAYER_NV_optimus=NVIDIA_only
+exec "$@"
