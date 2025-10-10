3827309f0e344f45f9b68ed2f2b148cdbcfea46a
Marcin Freitag <marcin.freitag@outlook.com>
2025-10-10 16:37:48 +0200

    fixed gensync-start.sh

--- private_dot_local/bin/executable_gensync-start.sh
+++ private_dot_local/bin/executable_gensync-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$(pgrep generalsync)" == "" ]; then
-	pushd ~/.local/bin/general-sync/generalsync
+	pushd ~/.local/bin/general-sync/
     ~/.local/bin/general-sync/generalsync --daemon &
     popd
     notify-send -t 2000 -i ~/.icons/generalsync.png "GeneralSync" "Daemon started"
