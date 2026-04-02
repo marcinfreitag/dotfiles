592e9d0f40639a32a3fd9c90b82efa1665c9da3e
Marcin Freitag <marcin.freitag@outlook.com>
2026-04-02 12:22:11 +0200

    replaced brightnessctl with light

--- private_dot_local/bin/hyprshell/executable_change-brightness.sh
+++ private_dot_local/bin/hyprshell/executable_change-brightness.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 notificationDuration=1000
 
-newBrightness="$(($(brightnessctl get -P) + $1))"
-
-brightnessctl set "$newBrightness%"
-newBrightness=$(brightnessctl get -P)
+newBrightness="$(($(printf "%.0f" $(light -G)) + $1))"
+light -S $newBrightness
+newBrightness=printf "%.0f" $(light -G)
 
 notify-send -e -i " " -h string:x-canonical-private-synchronous:osd -t $notificationDuration -u low "󰖨  $newBrightness%"
